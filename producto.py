23cbdf1bd760e2d6a0f0ca188eedcaf04248086f
Adrian <adrian.miranda.r@gmail.com>
2021-07-04 21:58:28 -0400

    Proto id_Method

--- producto.py
+++ producto.py
@@ -3,14 +3,19 @@ class Producto:
         self.nombre = nombre
         self.precio = precio
         self.categoria = categoria
+        self.id = 1
     
+    # def identificador(self ,  identificador):
+    #     for index in self.id:
+    #         self.id += 1
+
     def actualizarPrecio(self , impuesto , incrementado):
         if incrementado == True:
             self.precio += (self.precio * impuesto)
         else:
             self.precio -= (self.precio * impuesto)
     def impresion(self):
-        return f'Producto: {self.nombre}\nCategoria: {self.categoria}\nPrecio: {self.precio}'
+        return f'Id: {self.id}\nProducto: {self.nombre}\nCategoria: {self.categoria}\nPrecio: {self.precio}'
 
 if __name__ == '__main__':
     platano = Producto('Platano', 100 , 'Frutas')
@@ -19,5 +24,8 @@ if __name__ == '__main__':
     print(platano.precio)
     platano.actualizarPrecio(0.20 , False)
     print(platano.precio)
+
+    platano2 = Producto('avion' , 100 , 'vehiculo')
+    print(platano2.impresion())
 
 # print(locals())
